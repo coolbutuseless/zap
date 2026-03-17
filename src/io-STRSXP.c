5a57bd3ec2247f46eac9296027206e78531e7a63
mike <mikefc@coolbutuseless.com>
2026-03-17 20:47:29 +1000

    simple end-to-end dict string working

--- src/io-STRSXP.c
+++ src/io-STRSXP.c
@@ -17,6 +17,7 @@
 #include "io-ctx.h"
 #include "io-STRSXP.h"
 #include "utils-packing-1bit.h"
+#include "utils-ints.h"
 
 #include "mph.h"
 
@@ -249,8 +250,9 @@ SEXP read_STRSXP_mega(ctx_t *ctx) {
 //   * a vector of integers with the index of each string in the dictionary
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define BUF_NA_PACKED    0
-#define BUF_RAW          1
-#define BUF_COMP         2
+#define BUF_DICT         1
+#define BUF_MEGA         2
+#define BUF_IDX          3
 
 #define MAX_DICT_SIZE    4
 
@@ -275,7 +277,8 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
   }
 
   // Prep room for the index
-  int32_t *dict_idx = malloc(len * sizeof(int32_t));
+  prepare_buf(ctx, BUF_IDX, len * sizeof(int32_t));
+  int32_t *dict_idx = (int32_t *)ctx->buf[BUF_IDX];
   if (dict_idx == NULL) Rf_error("write_STRSXP_dict_(): Couldn't initialise integer idx");
   
   for (int i = 0; i < len; i++) {
@@ -302,7 +305,6 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if (!can_use_dict) {
     Rprintf("STRSXP: Using MEGA string\n");
-    free(dict_idx);
     mph_destroy(mph);
     write_STRSXP_mega(ctx, x_);
     return;
@@ -314,8 +316,9 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   Rprintf("STRSXP: Using Dict\n");
   uint64_t total_chars = 0;
-  char **dict = calloc(mph->nitems, sizeof(char *));
-  if (dict == 0) Rf_error("dict allocation error");
+
+  prepare_buf(ctx, BUF_DICT, mph->nitems * sizeof(char *));
+  char **dict = (char **)ctx->buf[BUF_DICT];
 
   for (int i = 0; i < mph->capacity; i++) {
     bucket_t b = mph->bucket[i];
@@ -334,8 +337,8 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
     Rprintf("[%i] %i\n", i, dict_idx[i]);
   }
   
-  prepare_buf(ctx, BUF_RAW, total_chars + 1);
-  char *dictp = (char *)ctx->buf[BUF_RAW];
+  prepare_buf(ctx, BUF_MEGA, total_chars + 1);
+  char *dictp = (char *)ctx->buf[BUF_MEGA];
   for (int i = 0; i < mph->nitems; i++) {
     Rprintf("[%i] -> %s\n", i, dict[i]);
 
@@ -343,8 +346,9 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
     Rprintf("slen: %lu\n", slen);
     strncpy(dictp, dict[i], slen);
     dictp += slen;  
+    free(dict[i]);
   }
-  dictp = (char *)ctx->buf[BUF_RAW];
+  dictp = (char *)ctx->buf[BUF_MEGA];
 
   for (int i = 0; i < total_chars; i++) {
     Rprintf("%02x ", dictp[i]);
@@ -366,26 +370,7 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
   write_uint8(ctx, STRSXP);
   write_uint8(ctx, ZAP_STR_DICT);
   write_len(ctx, len);
-  
-  
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Find lengths of all strings. (Including NULL byte)
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  total_chars = 0;
-  for (int i = 0; i < len; i++) {
-    total_chars += ((uint64_t)Rf_length(STRING_ELT(x_, i)) + 1); // count zero bytes
-  }
-  
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // length of mega should be represented by 8 bytes possibly?
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if (total_chars > (1ULL << 32)) Rf_error("write_STRSXP(): string too long");
-  
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Total Chars
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   write_len(ctx, total_chars);
-  if (total_chars == len) return; // all empty strings
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Create the auxilliary bitstream of NA locations
@@ -394,42 +379,29 @@ void write_STRSXP_dict(ctx_t *ctx, SEXP x_) {
   write_buf(ctx, BUF_NA_PACKED, packed_len);
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Allocate storage space for the entire long string
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  prepare_buf(ctx, BUF_RAW, total_chars + 1);
-  char *p = (char *)ctx->buf[BUF_RAW];
-  
-  
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Create the mega concatenated string
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  for (int i = 0; i < len; i++) {
-    SEXP chr_ = STRING_ELT(x_, i);
-    unsigned long slen = (unsigned long)Rf_length(chr_) + 1;
-    strncpy(p, CHAR(chr_), slen);
-    p += slen;
-  }
-  
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Output character data
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  write_buf(ctx, BUF_RAW, (size_t)total_chars);
+  write_len(ctx, mph->nitems);
+  write_buf(ctx, BUF_MEGA, (size_t)total_chars);
+  write_uint32_buf(ctx, BUF_IDX, len);
 
-  for (int i = 0; i < mph->nitems; i++) {
-    free(dict[i]);
-  }  
-  free(dict);
-  free(dict_idx);
   mph_destroy(mph);
 }
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Read STRSXP encoded as a dictionary lookup
+// Write:
+//  * [1] SEXP
+//  * [1] encoding method
+//  * [v] number of strings
+//  * [v] length of mega string (total_chars)
+//  * [bit] boolean vector of location of NA values
+//  * [chr] mega string 
+//  * [int] integer vector
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SEXP read_STRSXP_dict(ctx_t *ctx) {
   
-  Rf_error("read_STRSXP_dict(): not done yet");
   size_t len = read_len(ctx);
   SEXP obj_ = PROTECT(Rf_allocVector(STRSXP, (R_xlen_t)len)); 
   
@@ -440,14 +412,9 @@ SEXP read_STRSXP_dict(ctx_t *ctx) {
   
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Character data
+  // Number of chars in mega-string for dictionary
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   size_t total_chars = read_len(ctx);
-  if (total_chars == len) {
-    // Character vector of empty strings
-    UNPROTECT(1);
-    return obj_;
-  }
   
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -457,20 +424,33 @@ SEXP read_STRSXP_dict(ctx_t *ctx) {
   
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Read compressed char data
+  // Read mega-string representing dictionary
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  int n_dict_strings = (int)read_len(ctx);
   read_buf(ctx, BUF_RAW);
   char *mega = (char *)ctx->buf[BUF_RAW];
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Partition the mega string into individual strings
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  for (int i = 0; i < len; i++) {
-    SET_STRING_ELT(obj_, i, Rf_mkChar(mega));
+  SEXP *dict_char = malloc(n_dict_strings * sizeof(SEXP));
+  for (int i = 0; i < n_dict_strings; i++) {
+    dict_char[i] = PROTECT(Rf_mkChar(mega));
     mega += strlen(mega) + 1;
   }
+
+  // Read the dict indices
+  read_uint32_buf(ctx, BUF_IDX);
   
-  
+  // Allocate the stirngs
+  uint32_t *dict_idx = (uint32_t *)ctx->buf[BUF_IDX];
+  for (int i = 0; i < len; i++) {
+    SET_STRING_ELT(obj_, i, dict_char[dict_idx[i]]);
+  }
+
+
+  UNPROTECT(n_dict_strings);
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Set NA values using the auxilliary NA bistream
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
